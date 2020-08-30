4bd9125ef7269334ec97bf96f2bf2914ecdc7873
Joe McCleery <mccleery.joseph@gmail.com>
2020-08-30 14:43:06 +0800

    master: -homepage stuffs

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,83 @@
 <template>
-  <div class="uk-section uk-light">
-    <div class="uk-container">
-      <h1>
-        Home
-      </h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
-      <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
+  <div>
+    <div class="uk-section uk-light">
+      <div class="uk-container">
+        <h1>
+          Robert Bowen's<br/>
+          <span>
+            Free to use*<br/>
+            Cellar Management System
+          </span>
+        </h1>
+      </div>
+    </div>
+
+    <div class="uk-section uk-light">
+      <div class="uk-container">
+        <div class="uk-flex uk-flex-center uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-match" uk-grid>
+
+          <div>
+            <div class="uk-card uk-card-primary uk-card-body">
+              <h4 class="uk-card-title">
+                Helping People Enjoy Wine
+              </h4>
+              <p>
+                Celebrated Wine Maker Robert Bowen's  simplified approach to wine collection and management.
+              </p>
+              <p>
+                With the Bowen Code, Robert Bowen has managed to distill 45 years of wine making and collecting knowledge in a system that anyone can understand and use.
+              </p>
+              <p>
+                Robert Bowen's proven rating system presented as carefully considered combination of colour and alphanumeric tags allows you to gain overview of the status of your wine collection at a single glance.
+              </p>
+              <p>
+                Robert has made the Bowen Code freely* available to the wine collecting public as his contribution to supporting the education and enjoyment of wine.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <div class="uk-card uk-card-primary uk-card-body">
+              <h4 class="uk-card-title">
+                Starting A Cellar
+              </h4>
+              <p>
+                Shifting from being simply a consumer of wine to a collector can be very rewarding. The challenge is how to get started.
+              </p>
+              <p>
+                Within this site Rob provides access to a range or industry backed resources and people that can start you on the journey of wine collection.
+              </p>
+              <p>
+                From cost effective racking, online education services and of course the Bowen Code approach to wine collecting.
+              </p>
+              <p>
+                There is one golden rule when starting a cellar. "Buy more wine than you drink"
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <div class="uk-card uk-card-primary uk-card-body">
+              <h4 class="uk-card-title">
+                Wine I Am Making This Vintage
+              </h4>
+              <p>
+                Shifting from being simply a consumer of wine to a collector can be very rewarding. The challenge is how to get started.
+              </p>
+              <p>
+                Within this site Rob provides access to a range or industry backed resources and people that can start you on the journey of wine collection.
+              </p>
+              <p>
+                From cost effective racking, online education services and of course the Bowen Code approach to wine collecting.
+              </p>
+              <p>
+                There is one golden rule when starting a cellar. "Buy more wine than you drink"
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,3 +87,10 @@ export default {
   name: 'Home'
 }
 </script>
+
+<style lang="scss">
+  h1 span {
+    font-size: 0.7em;
+    line-height: 1;
+  }
+</style>
