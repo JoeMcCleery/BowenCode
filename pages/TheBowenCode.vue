9d5ca9544e8e942889d1d38de93110a83053e951
JoeMcCleery <mccleery.joseph@gmail.com>
2020-10-10 13:51:49 +0800

    -no user select on bottle neck tags

--- pages/TheBowenCode.vue
+++ pages/TheBowenCode.vue
@@ -16,13 +16,13 @@
 
           <div class="uk-card uk-card-primary uk-margin-auto uk-width-medium">
             <div class="uk-padding-small">
-              <a uk-tooltip="BowenCode URL" class="uk-flex uk-flex-center">Bowencode.com.au</a>
+              <a uk-tooltip="BowenCode URL" class="uk-flex uk-flex-center no-select">Bowencode.com.au</a>
 
-              <a uk-tooltip="Hole for bottle neck" class="uk-flex uk-flex-center uk-flex-middle uk-margin uk-height-small uk-width-small uk-margin-auto uk-background-secondary" style="border-radius: 50%;"></a>
+              <a uk-tooltip="Hole for bottle neck" class="uk-flex uk-flex-center uk-flex-middle uk-margin uk-height-small uk-width-small uk-margin-auto uk-background-secondary no-select" style="border-radius: 50%;"></a>
 
               <div class="uk-grid-small uk-grid-match" uk-grid>
                 <div class="uk-width-1-3">
-                  <a class="uk-height-1-1 uk-flex uk-flex-center uk-flex-middle" uk-tooltip="Barcode">
+                  <a class="uk-height-1-1 uk-flex uk-flex-center uk-flex-middle no-select" uk-tooltip="Barcode">
                     <img :src='Barcode' alt="Barcode" uk-img/>
                   </a>
                 </div>
@@ -30,7 +30,7 @@
                 <div class="uk-width-2-3">
                   <div class="uk-grid-small uk-grid-match" uk-grid>
                     <div>
-                      <a class="uk-flex uk-flex-column" uk-tooltip="Wine Score / Price">
+                      <a class="uk-flex uk-flex-column no-select" uk-tooltip="Wine Score / Price">
                         <span><span uk-icon="star"/><span uk-icon="star" /><span uk-icon="star" /><span uk-icon="star" /><span uk-icon="star" /></span>
                         <span><span uk-icon="tag" /> $XXX.xx</span>
                       </a>
@@ -39,14 +39,14 @@
                     <div class="uk-width-1-1">
                       <div class="uk-grid-small uk-flex uk-flex-between" uk-grid>
                         <div class="uk-width-2-3">
-                          <a class="uk-flex uk-flex-column" uk-tooltip="Drinking Window">
+                          <a class="uk-flex uk-flex-column no-select" uk-tooltip="Drinking Window">
                             <sup>Window</sup>
                             <span>01/12/20</span>
                           </a>
                         </div>
 
                         <div class="uk-width-1-3 wine-type-colour" style="position: relative; top: -12px; height: 38px;">
-                          <a class="uk-flex uk-flex-column" uk-tooltip="Wine Type / Color">
+                          <a class="uk-flex uk-flex-column no-select" uk-tooltip="Wine Type / Color">
                             <span style="position: relative; top: 7px;">Char</span>
                           </a>
                         </div>
@@ -56,14 +56,14 @@
                     <div class="uk-width-1-1">
                       <div class="uk-grid-collapse uk-grid-match uk-position-relative" uk-grid>
                         <div class="uk-width-1-1">
-                          <a class="uk-flex uk-flex-column" uk-tooltip="Wine Details">
+                          <a class="uk-flex uk-flex-column no-select" uk-tooltip="Wine Details">
                             <sup>Details</sup>
                             RnB Chardonnay 2019
                           </a>
                         </div>
 
                         <div class="uk-position-bottom-right">
-                          <a uk-tooltip="Cut Off Corner When Ready To Drink">
+                          <a class="no-select" uk-tooltip="Cut Off Corner When Ready To Drink">
                             <span class="cut-here-line" />
                           </a>
                         </div>
