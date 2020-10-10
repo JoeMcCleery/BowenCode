07eb429f2d4033fe0d7d63c50a8c31475c2d1cdd
JoeMcCleery <mccleery.joseph@gmail.com>
2020-10-10 17:04:40 +0800

    -gradient updates

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="uk-container">
       <div class="uk-padding uk-padding-remove-vertical">
         <p>
-          *Yes it really is free to use, all we ask is that you acknowledge us as the owners of the IP and you provide a link to Bowencode.com.au |
+          *Yes it really is free to use, all we ask is that you acknowledge us as the owners of the IP and you provide a link to Bowencode.com.au
           This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit the
           <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/" title="Share Alike License" class="uk-link-text">creative commons website</a>.
         </p>
